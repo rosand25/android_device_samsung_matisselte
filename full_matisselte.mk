339f759401795a752a55e92b2d24208138fb0bb2
Daniel <rosand25@gmail.com>
2017-03-10 17:13:24 -0500

    Update full_matisselte.mk

--- full_matisselte.mk
+++ full_matisselte.mk
@@ -1,10 +1,11 @@
-# Copyright (C) 2016 The CyanogenMod Project
+#
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,3 +25,6 @@ PRODUCT_NAME := full_matisselte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T535
 PRODUCT_MANUFACTURER := samsung
+
+$(call inherit-product, device/samsung/matisselte/device.mk)
+$(call inherit-product-if-exists, vendor/samsung/matisse3g/matisselte-vendor.mk)
